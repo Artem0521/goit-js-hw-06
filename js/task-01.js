8f17811d86293a7345008fe6a930e0fed2b8000f
Artem0521 <artemgorbachov845@gmail.com>
2022-10-12 14:28:16 +0300

    правки

--- js/task-01.js
+++ js/task-01.js
@@ -16,48 +16,4 @@ categories.forEach(category => {
     });
 })
 
-// const categoryLength = [...category.children];
-
-
-// for (const category of categories) {
-//     if(category.tagName === 'H2'){
-//         console.log('Category: ', category.textContent);
-//     }
-// }
-
-
-
-
-
-// const categoryNumbers = document.querySelectorAll('h2');
-
-// for (const category of categoryNumbers) {
-//     if (category === 'H2') {
-//         return 
-//     }
-//     console.log(category.textContent);
-// }
-
-// const categories = document.querySelectorAll('.item');
-
-// console.log('Number of Categories: ', categories.length);
-
-// categories.forEach(category =>{
-//     const group = [...category.children];
-
-//     group.forEach(element => {
-//         if (element.TagName === 'H2'){
-//         console.log('Category: ', element.textContent);
-//         }
-//     });
-
-//     group.forEach(element => {
-//         if(element.TagName === 'UL') {
-//             console.log('Elements: ', element.length);
-//         }
-//     });
-//     console.log('');
-
-// });
-
 
