ee90aea62a9fd484d3fddf0eaa61d32b7727852f
Artem0521 <artemgorbachov845@gmail.com>
2022-10-14 09:43:39 +0300

    update task 7

--- js/task-07.js
+++ js/task-07.js
@@ -1,9 +1,16 @@
 
-const inputChange = document.querySelector('input#font-size-control');
-const textAbra = document.querySelector('span#text');
+// const inputChange = document.querySelector('input#font-size-control');
+// const textAbra = document.querySelector('span#text');
 
-inputChange.addEventListener('change', event => {
-    textAbra.style.fontSize = `${event.target.value}px`;
+// inputChange.addEventListener('change', event => {
+//     textAbra.style.fontSize = `${event.target.value}px`;
+// })
+
+
+const inputCh = document.querySelector('input#font-size-control')
+const abra = document.querySelector('span#text')
+
+inputCh.addEventListener('input', e => {
+    abra.style.fontSize = `${e.target.value }px`
 })
-
 
